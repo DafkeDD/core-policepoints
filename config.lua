7dfd3a820eb43acafc194934cccfc303164c94be
core-store <104695448+core-store@users.noreply.github.com>
2023-07-09 14:40:38 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,3 @@
+Config = {}
+Config.time = 15
+Config.points = 1 --how many points the player gets after  the  Config.time
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'core policepoints  made by AL!#1809'
+
+shared_script 'config.lua'
+client_script 'client/c_main.lua'
+server_script 'server/s_main.lua'
+
+lua54 'yes'
