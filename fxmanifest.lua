9bf70b05ca43747207c3fa60fb1fb56cd9f80a68
core-store <104695448+core-store@users.noreply.github.com>
2023-07-09 14:42:25 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 description 'core policepoints  made by AL!#1809'
 
 shared_script 'config.lua'
-client_script 'client/c_main.lua'
-server_script 'server/s_main.lua'
+client_script 'c_main.lua'
+server_script 's_main.lua'
 
 lua54 'yes'
